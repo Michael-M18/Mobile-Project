59b3b87a418b3842d0744fb1dae3789673d0bc9e
Michael <MM3861@live.mdx.ac.uk>
2022-12-08 13:32:33 -0800

    My Current Project

--- Mobile-Game/Assets/Player/Movement.cs
+++ Mobile-Game/Assets/Player/Movement.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float speed = 5f;
+    public float Movementspeed = 5f;
     // Update is called once per frame
     void Update()
     {
-        float xDirection = Input.GetAxis("Vertical");
-        float zDirection = Input.GetAxis("Horizontal");
+        float xDirection = Input.GetAxis("Horizontal");
+        float zDirection = Input.GetAxis("Vertical");
 
         Vector3 moveDirection = new Vector3(xDirection, 0.0f, zDirection);
 
